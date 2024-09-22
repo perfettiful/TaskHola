8fe018ae9490840fd568462b80634329ac61054d
Nathan Perfetti <nathanperfetti@gmail.com>
2024-09-22 12:55:05 -0500

    add appetize deploy actio

--- backend/database/init-tasks-db.sql
+++ backend/database/init-tasks-db.sql
@@ -7,15 +7,9 @@ CREATE TABLE tasks (
 );
 
 INSERT INTO tasks (task_description, user_email) VALUES
-('Signup for TaskHola ✅', 'joe.schmo@go.co'),
-('Plan a weekend getaway 🏖️', 'jane.doe@example.com'),
-('Learn to juggle 🤹', 'circus.fan@example.com'),
-('Bake a rainbow cake 🌈🍰', 'sweet.tooth@example.com'),
+('Try a new recipe 🍳', 'foodie.adventurer@example.com'),
 ('Start a herb garden 🌿', 'green.thumb@example.com'),
 ('Write a short story 📝', 'aspiring.author@example.com'),
-('Learn a magic trick 🎩✨', 'wannabe.magician@example.com'),
-('Host a game night 🎲🃏', 'party.planner@example.com'),
-('Try a new cuisine 🍜', 'foodie.explorer@example.com'),
-('Take a dance class 💃', 'rhythm.enthusiast@example.com'),
-('Build a birdhouse 🐦🏠', 'nature.lover@example.com'),
-('Learn to play ukulele 🎸', 'music.newbie@example.com');
+('Visit a new museum 🖼️', 'culture.vulture@example.com'),
+('Learn to play ukulele 🎸', 'music.newbie@example.com'),
+('Take a yoga class 🧘‍♀️', 'yoga.enthusiast@example.com');
