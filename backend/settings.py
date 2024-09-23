9d9b8e89e253ddf34032718c10aeb48b61f64ba5
Nathan Perfetti <nathanperfetti@gmail.com>
2024-09-23 00:37:58 -0500

    cleanup env vars

--- backend/settings.py
+++ backend/settings.py
@@ -3,10 +3,10 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'my_db'),
-        'USER': os.environ.get('DB_USER', 'wittcepter'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'theBestChromeExtension'),
-        'HOST': os.environ.get('DB_HOST', 'database'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
